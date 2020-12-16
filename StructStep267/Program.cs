a66b4777c57585b9e31ddaff1edf8bafa1bfdd28
student <student@learncodinganywhere.com>
2020-12-15 17:41:28 -0800

    Add project files.

---
+++ StructStep267/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructStep267
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number( 50.0m);
+            Console.WriteLine("  debt  Amount = {0} ",number.Amount);
+            Console.ReadLine();
+           
+        }
+        
+        
+ 
+    }
+   
+}
